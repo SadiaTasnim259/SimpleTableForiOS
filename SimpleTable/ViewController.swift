a1aac0a69b9726c1882e16644acf96141f175e2b
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-03-05 11:44:26 +0600

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Sadia on 23/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let cellIdentifier = "datacell"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var prefersStatusBarHidden: Bool {true}
+    
+
+    var restaruantNames = [
+            "Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"
+    ]
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaruantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = restaruantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "restaurant")
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate{
+    
+}
+
